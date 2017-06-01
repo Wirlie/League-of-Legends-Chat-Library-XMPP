09111b8a2ac5e4f900d2c5b81b9cf1314f894618
Wirlie <josue.211996@outlook.com>
2017-05-31 21:46:27 -0500

    Add remaining regions (some regions are still pending to add)

--- LolXMPPAPI/src/lolxmpp/api/ChatRegion.java
+++ LolXMPPAPI/src/lolxmpp/api/ChatRegion.java
@@ -19,8 +19,54 @@
 package lolxmpp.api;
 
 public enum ChatRegion {
-	
+	/**
+	 * Brazil
+	 */
+	BR("chat.br.lol.riotgames.com"),
+	/**
+	 * Europe Nordic and East
+	 */
+	EUNE("chat.eun1.lol.riotgames.com"),
+	/**
+	 * Europe West
+	 */
+	EUW("chat.euw1.lol.riotgames.com"),
+	/**
+	 * Korea
+	 */
+	KR("chat.kr.lol.riotgames.com"),
+	/**
+	 *  Latin America North
+	 */
 	LAN("chat.la1.lol.riotgames.com"),
+	/**
+	 *  Latin America South
+	 */
+	LAS("chat.la2.lol.riotgames.com"),
+	/**
+	 * North-America
+	 */
+	NA("chat.na1.lol.riotgames.com"),
+	/**
+	 * North-America #2
+	 */
+	NA2("chat.na2.lol.riotgames.com"),
+	/**
+	 * Oceania
+	 */
+	OCE("chat.oc1.lol.riotgames.com"),
+	/**
+	 * Public Beta Environment
+	 */
+	PBE("chat.pbe1.lol.riotgames.com"),
+	/**
+	 * Russia
+	 */
+	RU("chat.ru.lol.riotgames.com"),
+	/**
+	 * Turkey
+	 */
+	TR("chat.tr.lol.riotgames.com")
 	;
 	
 	private String serverHost;
