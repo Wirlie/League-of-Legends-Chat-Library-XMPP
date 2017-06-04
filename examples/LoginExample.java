b64c057e461b3f9e97c88f7133d02f21dc632813
Josue Acevedo <josue.211996@outlook.com>
2017-06-04 15:06:42 -0500

    Login Example

---
+++ examples/LoginExample.java
@@ -0,0 +1,25 @@
+public class LoginExample {
+	
+	public static void main(String[] arguments) {
+		LoLXMPPAPI api = new LoLXMPPAPI(ChatRegion.NA, new RiotAPI("RGAPI-XXX-XXX-XXX-XXX-XXX"));
+		
+		String lolUsername = "Wirlie";
+		String lolPassword = "xxxxxxxxx";
+		
+		if(api.login(lolUsername, lolPassword)) {
+			//Wait the API for the "it's ready for use" status
+			api.onReady(() -> {
+				...
+			});
+		} else {
+			if(api.getLoginResult() == LoginResult.AUTH_FAILED) {
+				//wrong username or password
+				...
+			} else {
+				//internal API error
+				...
+			}
+		}
+	}
+
+}
